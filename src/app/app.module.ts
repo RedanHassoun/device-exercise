6e02793e59d0e5c036c3b909ff1232f1de4a374b
RedanHassoun <redan.has.1991@gmail.com>
2018-08-15 23:45:47 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SelectBlackboxComponent } from './select-blackbox/select-blackbox.component';
+import { SelectBlackboxService } from './services/select-blackbox.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectBlackboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [SelectBlackboxService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
